cf75aff14adce2b06667f618c8bae79ecf93c9d8
Сергей <s.harlov@blanc.ru>
2022-03-14 01:48:29 +0500

    svg animation + page 404

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './index.scss';
+import {Route, Router, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
+import PageNotFound from "./pages/page-not-found/Page-not-found";
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <Routes>
+        <Route path="/" element={<React.Suspense fallback={<>...</>}><Home /></React.Suspense>} />
+        <Route path="*" element={<React.Suspense fallback={<>...</>}><PageNotFound /></React.Suspense>} />
+    </Routes>
   );
 }
 
---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return <>
+        <h1>hello world 2</h1>
+    </>
+}
+
+export default Home;
