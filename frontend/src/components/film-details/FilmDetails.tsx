b13f03f3161c78ffd639e68001f582273b44fb3a
Сергей <s.harlov@blanc.ru>
2022-03-21 03:54:21 +0500

    card hover

--- frontend/src/components/film-details/FilmDetails.tsx
+++ frontend/src/components/film-details/FilmDetails.tsx
@@ -6,6 +6,9 @@ const FilmDetails: FC<FilmDetailsProps> = ({close}) => {
     return (
         <div className="film-details">
             {close && <button onClick={close}>Закрыть</button>}
+            <div className="film-details__background">
+                <div className="film-details__image" style={{backgroundImage: "url(https://avatars.mds.yandex.net/get-ott/212840/2a000001628b1c50596bd4462d7349dcb9e9/2016x1134)"}} />
+            </div>
         </div>
     )
 }
